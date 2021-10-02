2b9c2bc3c4008893f4191f7e4bb3f90f73f061d7
Nathan <chuck199411@gmail.com>
2021-10-02 22:05:08 +0800

    对数器、局部最小值、二分查找

--- src/main/java/c1_sort/InsertionSorting.java
+++ src/main/java/c1_sort/InsertionSorting.java
@@ -1,18 +1,49 @@
 package c1_sort;
 /*
-* 插入排序
-* 4，7，2，3，100，17，24
-* 这样的一个数组，依次的从左往右，选择
-* 4 7 排序
-* 4 7 2 排序  247
-* 2 4 7 3 排序 2 3 4 7
-* 所以最坏情况 是On²  但是，最好情况 123456 是On
-* 算法的时间复杂度按照最坏的情况来 O 这种的
-*
-*
-*
-* */
-
+ * 插入排序
+ * 4，7，2，3，100，17，24
+ * 这样的一个数组，依次的从左往右，选择
+ * 4 7 排序
+ * 4 7 2 排序  247
+ * 2 4 7 3 排序 2 3 4 7
+ * 所以最坏情况 是On²  但是，最好情况 123456 是On
+ * 算法的时间复杂度按照最坏的情况来 O 这种的.
+ * 而 冒泡排序 选择排序必然是O n²
+ *
+ * */
 
 public class InsertionSorting {
+
+    public static void insertionSort(int[] arr) {
+
+        if (arr == null || arr.length < 1) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 78, 1, 4, 5, 7};
+        insertionSort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
 }
---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,47 @@
+package search;
+
+/*
+ * 在有序数组中，查找某个数是否存在。
+ *
+ * 时间复杂度 log 2（N） --> 2为底数，假设一共有16个数，8 4 2 1 一共需要判断4次
+ * 由于可以忽略常数，可以直接写成 logN
+ *
+ * 有序数组 查找大于等于 某个数 最左侧的位置
+ *
+ *
+ * 局部最小值问题
+ * 在 无顺序的 数组中，相邻的数一定不相等 ，求局部最小值
+ * 局部最小值：比左右两边的数都要小
+ *
+ * 如果开头是局部最小，就是向下的折线。如果尾部是局部最小就是向下的
+ * 这时候使用二分
+ *
+ * */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 78, 88};
+        System.out.println(binarySearch(arr, 78));
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int val = nums[mid];
+
+            if (target == val) {
+                return mid;
+
+            } else if (target < val) {
+
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+}
